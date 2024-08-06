f940fbde67de2b307b55f33a5d2043b2e515cedc
GenivaldoSerra <genivaldo.serra@live.com>
2024-08-06 11:06:02 -0300

    feat: Aula 05

--- migrations/versions/6ef3bfc71b1e_create_users_table.py
+++ migrations/versions/6ef3bfc71b1e_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: 0bb3f39453a7
+Revision ID: 6ef3bfc71b1e
 Revises: 
-Create Date: 2024-07-30 12:06:55.438574
+Create Date: 2024-07-31 10:29:12.877793
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '0bb3f39453a7'
+revision: str = '6ef3bfc71b1e'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -26,6 +26,7 @@ def upgrade() -> None:
     sa.Column('password', sa.String(), nullable=False),
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('created_at', sa.DateTime(), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=False),
+    sa.Column('update_at', sa.DateTime(), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_api.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -29,18 +31,10 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'testeusername',
-                'email': 'teste@test.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -56,17 +50,14 @@ def test_update_user(client):
     }
 
 
-def test_read_user_id(client):
-    response = client.get('/users/1')
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
 
-    assert response.json() == {
-        'id': 1,
-        'username': 'testeuser',
-        'email': 'teste1@test.com',
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.json() == {'message': 'Usuário deletado!'}
